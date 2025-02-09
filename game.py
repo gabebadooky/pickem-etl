0f9f28310be1578511b6361c215c1fa4f2e3b28e
gbaduqui <baduquig@gmail.com>
2025-02-09 13:44:31 -0700

    Develop get_game_data() method

---
+++ game.py
@@ -0,0 +1,13 @@
+import requests
+
+game = 401754516
+
+
+def get_game_data(game_id: str) -> dict:
+    """
+    Method that makes GET request to ESPN College Football Schedule
+    endpoint for a given Game ID and returns the JSON response
+    """
+    espn_game_page = f'http://site.api.espn.com/apis/site/v2/sports/football/college-football/summary?event={game_id}'
+    json_response = requests.get(espn_game_page).json()
+    return json_response
