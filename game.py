a8fa98c6073f412b90a2138756629a957a9a722a
gbaduqui <baduquig@gmail.com>
2025-02-10 17:03:31 -0700

    Parse zulu_datetime, game_date and broadcast strings from JSON response

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import requests
 
-game = 401754516
+game = '401754516'
 
 
 def get_game_data(game_id: str) -> dict:
@@ -11,3 +11,12 @@ def get_game_data(game_id: str) -> dict:
     espn_game_page = f'http://site.api.espn.com/apis/site/v2/sports/football/college-football/summary?event={game_id}'
     json_response = requests.get(espn_game_page).json()
     return json_response
+
+
+game_json = get_game_data(game)
+
+zulu_datetime = game_json['header']['competitions'][0]['date'].split('T')[1]
+game_date = game_json['header']['competitions'][0]['date'].split('T')[0]
+broadcast = game_json['header']['competitions'][0]['broadcasts']
+
+print(broadcast)
