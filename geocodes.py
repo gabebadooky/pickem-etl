fbb066d372ac4d2ff2ed89d2cac1e1bbad6b08c7
gbaduqui <baduquig@gmail.com>
2025-02-20 13:18:58 -0700

    Refactor geocodes.py to handle api call in one method and return lat/lon tuple in the other

--- geocodes.py
+++ geocodes.py
@@ -1,10 +1,7 @@
 import requests
 from open_cage_api_key import key
 
-def extract_latitude(data: dict) -> str:
-    return ''
-
-def get_geocode(stadium: str, city: str, state=None) -> tuple:
+def call_geocode_api(stadium: str, city: str, state=None) -> dict:
     open_cage_geocode_endpoint = f'https://api.opencagedata.com/geocode/v1/json?key={key}'
     formatted_stadium = stadium.replace(' ', '+')
     formatted_city = city.replace(' ', '+')
@@ -13,10 +10,11 @@ def get_geocode(stadium: str, city: str, state=None) -> tuple:
         open_cage_geocode_endpoint = f'{open_cage_geocode_endpoint}&q={formatted_stadium}+{formatted_city}+{formatted_state}'
     else:
         open_cage_geocode_endpoint = f'{open_cage_geocode_endpoint}&q={formatted_stadium}+{formatted_city}'
-
     data = requests.get(open_cage_geocode_endpoint).json()
+    return data
 
+def get_lat_long_tuple(stadium: str, city: str, state=None) -> tuple:
+    data = call_geocode_api(stadium, city, state)
     latitude = data['results'][0]['geometry']['lat']
     longitude = data['results'][0]['geometry']['lng']
-
     return latitude, longitude
