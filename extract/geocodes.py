fe6ab31e61fefad498f18157cfb20d2dea6158e9
gbaduqui <baduquig@gmail.com>
2025-02-25 15:42:55 -0700

    Implement error handling on OpenCage API extract methods

--- extract/geocodes.py
+++ extract/geocodes.py
@@ -3,18 +3,27 @@ from credentials.open_cage_api_key import key
 
 def call_geocode_api(stadium: str, city: str, state=None) -> dict:
     open_cage_geocode_endpoint = f'https://api.opencagedata.com/geocode/v1/json?key={key}'
-    formatted_stadium = stadium.replace(' ', '+')
+    formatted_stadium = stadium.replace(' ', '+').replace('&', '')
     formatted_city = city.replace(' ', '+')
     if all(state):
         formatted_state = state.replace(' ', '+')
         open_cage_geocode_endpoint = f'{open_cage_geocode_endpoint}&q={formatted_stadium}+{formatted_city}+{formatted_state}'
     else:
         open_cage_geocode_endpoint = f'{open_cage_geocode_endpoint}&q={formatted_stadium}+{formatted_city}'
-    data = requests.get(open_cage_geocode_endpoint).json()
+    try:
+        data = requests.get(open_cage_geocode_endpoint).json()
+    except:
+        data = { 'results': [ { 'geometry': { 'lat': 0, 'lng': 0 } } ] }
     return data
 
 def get_lat_long_tuple(stadium: str, city: str, state=None) -> tuple:
     data = call_geocode_api(stadium, city, state)
-    latitude = data['results'][0]['geometry']['lat']
-    longitude = data['results'][0]['geometry']['lng']
+    print(f'{stadium}, {city}, {state}\nGeocode: {data}')
+
+    try:
+        latitude = data['results'][0]['geometry']['lat']
+        longitude = data['results'][0]['geometry']['lng']
+    except:
+        latitude = 0
+        longitude= 0
     return latitude, longitude
