5e71004e1dd0dadd73954d046f89e4db4b243377
gbaduqui <baduquig@gmail.com>
2025-02-25 15:41:20 -0700

    Call all required methods to complete ESPN ETL

--- cfb_etl.py
+++ cfb_etl.py
@@ -1,61 +1,46 @@
+import extract.espn.endpoint_data as e
+import transform.transform as t
+import load.mysql_db as ld
 import requests
-import game as g
 
+weeks = 1
+distinct_away_teams = set()
 
-def get_week_games(week_num: int) -> list:
+def get_all_games_in_week(week_num: int) -> list:
     espn_scoreboard_endpoint = f'https://site.api.espn.com/apis/site/v2/sports/football/college-football/scoreboard?seasontype=2&week={week_num}'
     data = requests.get(espn_scoreboard_endpoint).json()
     events = data['events']
-    week_games = []
+    return events
 
-    for event in events:
-        game_id = f'{g.extract_away_team(event)}-{g.extract_home_team(event)}-{g.extract_gamedate(event)}'
+def get_team(team_id: str) -> dict:
+    espn_team_endpoint = f'http://site.api.espn.com/apis/site/v2/sports/football/college-football/teams/{team_id}'
+    data = requests.get(espn_team_endpoint).json()
+    return data
 
-        game = dict(
-            # game
-            game_id = game_id,
-            league = 'CFB',
-            week = week_num,
-            cbs_code = '',
-            espn_code = event['id'],
-            fox_code = '',
-            vegas_code = '',
-            away_team_id = g.extract_away_team(event),
-            home_team_id = g.extract_home_team(event),
-            date = g.extract_gamedate(event),
-            time = g.extract_datetime(event),
-            tv_coverage = g.extract_broadcast(event),
-            game_finished = g.extract_game_finished(event),
+def load_game_data(game_data: dict):
+    ld.load_game(t.format_game(game_data))
+    ld.load_odds(t.format_odds(game_data))
+    ld.load_location(t.format_location(game_data))
+    ld.load_box_scores(t.format_away_box_score(game_data))
+    ld.load_box_scores(t.format_home_box_score(game_data))
 
-            # box score
-            away_q1_score = 0,
-            away_q2_score = 0,
-            away_q3_score = 0,
-            away_q4_score = 0,
-            away_overtime_score = 0,
-            away_total_score = 0,
-            home_q1_score = 0,
-            home_q2_score = 0,
-            home_q3_score = 0,
-            home_q4_score = 0,
-            home_overtime_score = 0,
-            home_total_score = 0,
+def load_team_data(team_data: dict):
+    ld.load_team(t.format_team(team_data))
+    ld.load_team_stats(t.format_team_stats(team_data))
+    ld.load_record(t.format_conference_record(team_data))
+    ld.load_record(t.format_overall_record(team_data))
 
-            # odds
-            source = 'ESPN',
-            away_moneyline = g.extract_away_moneyline(event),
-            home_moneyline = g.extract_home_moneyline(event),
-            away_spread = g.extract_away_spread(event),
-            home_spread = g.extract_home_spread(event),
-            over_under = g.extract_over_under(event),
-            away_win_percentage = '',
-            home_win_percentage = '',
+# Extract and Load Game related data
+for week in range(weeks):
+    week += 1
+    games = get_all_games_in_week(week)
+    for game in games:
+        game_dict = e.get_scoreboard_data(game)
+        distinct_away_teams.add(game_dict['away_team_id'])
+        load_game_data(game_dict)
 
-            # location
-            stadium = g.extract_stadium(event),
-            city = g.extract_city(event),
-            state = g.extract_state(event),
-        )
-        week_games.append(game)
+# Extract and Load Team related data
+for team in distinct_away_teams:
+    team_dict = e.get_team_data(team)
+    load_team_data(team_dict)
 
-    return week_games
