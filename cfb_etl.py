0e6a12525b8e6f59fcbf1b44482320d8e28eab0c
gbaduqui <baduquig@gmail.com>
2025-02-26 16:02:32 -0700

    Enhance code to handle Game and Team objects

--- cfb_etl.py
+++ cfb_etl.py
@@ -1,4 +1,5 @@
-import extract.espn.endpoint_data as e
+import extract.espn.game as eg
+import extract.espn.team as et
 import transform.transform as t
 import load.mysql_db as ld
 import requests
@@ -17,14 +18,14 @@ def get_team(team_id: str) -> dict:
     data = requests.get(espn_team_endpoint).json()
     return data
 
-def load_game_data(game_data: dict):
+def load_game_data(game_data: object):
     ld.load_game(t.format_game(game_data))
     ld.load_odds(t.format_odds(game_data))
     ld.load_location(t.format_location(game_data))
     ld.load_box_scores(t.format_away_box_score(game_data))
     ld.load_box_scores(t.format_home_box_score(game_data))
 
-def load_team_data(team_data: dict):
+def load_team_data(team_data: object):
     ld.load_team(t.format_team(team_data))
     ld.load_team_stats(t.format_team_stats(team_data))
     ld.load_record(t.format_conference_record(team_data))
@@ -33,14 +34,17 @@ def load_team_data(team_data: dict):
 # Extract and Load Game related data
 for week in range(weeks):
     week += 1
-    games = get_all_games_in_week(week)
-    for game in games:
-        game_dict = e.get_scoreboard_data(game)
-        distinct_away_teams.add(game_dict['away_team_id'])
-        load_game_data(game_dict)
+    week_json_response = get_all_games_in_week(week)
+    for game_json in week_json_response:
+        game = eg.Game(game_json, 'CFB')
+        print(game.date)
+        #game_dict = e.get_scoreboard_data(game)
+        distinct_away_teams.add(game.away_team_id)
+        load_game_data(game)
 
 # Extract and Load Team related data
-for team in distinct_away_teams:
-    team_dict = e.get_team_data(team)
-    load_team_data(team_dict)
+for distict_team in distinct_away_teams:
+    #team_dict = e.get_team_data(team)
+    team = et.Team(distict_team)
+    load_team_data(team)
 
